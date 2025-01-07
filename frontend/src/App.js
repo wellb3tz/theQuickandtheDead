5901c88843f8a9bd2f2972b5e5dc8983c4975bd3
wellb3tz <wellb3tz@gmail.com>
2025-01-07 17:34:15 +0300

    Developing (fixing gh-pages 4)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import InitialScreen from './components/InitialScreen';
 import Register from './components/Register';
 import Login from './components/Login';
@@ -8,11 +8,11 @@ function App() {
   return (
     <Router>
       <div className="App">
-        <Switch>
-          <Route path="/" exact component={InitialScreen} />
-          <Route path="/register" component={Register} />
-          <Route path="/login" component={Login} />
-        </Switch>
+        <Routes>
+          <Route path="/" element={<InitialScreen />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
       </div>
     </Router>
   );
