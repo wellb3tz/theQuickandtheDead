a35606c9ffa3a972139031c2b6726aafcd701cf7
wellb3tz <wellb3tz@gmail.com>
2025-01-16 01:13:51 +0300

    dev (particle system 6)

--- frontend/src/components/ParticleSystem.js
+++ frontend/src/components/ParticleSystem.js
@@ -9,23 +9,26 @@ const ParticleSystem = forwardRef(({ scene }, ref) => {
 
   useEffect(() => {
     const createNebula = async () => {
-      const particleTexture = new THREE.TextureLoader().load('https://raw.githubusercontent.com/wellb3tz/theQuickandtheDead/main/frontend/media/particle2.png');
-      const particleMaterial = new THREE.SpriteMaterial({ map: particleTexture });
+      const particleMaterial = new THREE.PointsMaterial({
+        color: 0xff0000,
+        size: 0.5, // Increase the size of the particles
+        sizeAttenuation: true,
+      });
 
       const emitter = new Emitter();
       emitter
-        .setRate(new Rate(new Span(10, 20), new Span(0.01)))
+        .setRate(new Rate(new Span(50, 100), new Span(0.01))) // Increase the quantity of particles
         .addInitializers([
           new Position(new THREE.Vector3(0, 0, 0)),
           new Mass(1),
-          new Radius(0.1, 0.5),
+          new Radius(0.5, 1), // Increase the radius of the particles
           new Life(1, 2),
           new Body(particleMaterial),
           new RadialVelocity(50, new THREE.Vector3(0, 1, 0), 180)
         ])
         .addBehaviours([
           new Color(new THREE.Color(0xff0000), new THREE.Color(0x000000)),
-          new Radius(0.5, 0.1)
+          new Radius(1, 0.5) // Increase the radius of the particles
         ]);
 
       const nebula = new Nebula();
